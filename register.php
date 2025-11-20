a4aa8bf70bc399ccc2f9972e2a44099f0831ea47
amcguir9 <amcguir9@my.epcc.edu>
2025-11-20 22:45:39 +0000

    fgfsg

---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Mini-Hackathon Registration Form</title>
+	</head>
+	<body>
+		<h1>Mini-Hackathon 2025 Registration Form</h1>
+		<form>
+			<label for="name">Name:</label>
+			<input id="name> type="text">
+&nbsp
+			<label for="secret">Secret Code:</label>
+			<input id="secret"> type="password">
+&nbsp			
+			<label for="review">Share your thoughts below!</label>
+			<textarea id="review" rows="10"></textarea>
+	</form>
+</body>	
+</html>
