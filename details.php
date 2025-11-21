a6169083522c4a836af4ced7598daa7f22a85192
amcguir9 <amcguir9@my.epcc.edu>
2025-11-21 05:29:40 +0000

    adfada

---
+++ details.php
@@ -0,0 +1,4 @@
+<?php
+$name = $_COOKIE["student_name"] ?? "guest";
+$track = $_COOKIE["student_track"] ?? "undecided";
+?>
--- submit.php
+++ submit.php
@@ -1,4 +1,24 @@
 <?php
+$name = $_POST["name"];   // match the input name=""
+$email = $_POST["email"];
+$track = $_POST["track"];
+$shirt = $_POST["shirt"];
+$laptop = isset($_POST["laptop"]) ? "yes" : "no";
+?>
+<?php
+setcookie("name", $name, time() + 60*60*24*7); // 7 days
+setcookie("track", $track, time() + 60*60*24*7);
+?>
+<?php
+$line = $name . "," . $email . "," . $track . "," . $shirt . "," . $laptop . "\n";​
 
+$file = fopen("registrations.txt", "a");
+fwrite($file, $line);
+fclose($file);
+?>
+<?php
+
+$message = "Thank you for registering $name! You are set for track $track .";
+<a href="index.html">Back to homepage</a>
 
 ?>
